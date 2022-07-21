f0a2a3d57fb59c954c28e67453e4e0cb16d5af3b
Stefan Bogdanovic <sbogdanovic@nanointeractive.com>
2022-07-21 11:12:41 +0200

    Github pages try #1

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,37 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	extensions: ['.svelte', '.md'],
+
+// 	kit: {
+// 		outDir: './docs',
+// 		adapter: adapter(),
+
+// 		prerender: {
+// 			default: true,
+// 			entries: ['*']
+// 		}
+// 	},
+// 	preprocess: preprocess({})
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
+const dev = process.env.NODE_ENV === 'development';
+const repo = '/FastEndpoints-DocSite';
+
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
 	extensions: ['.svelte', '.md'],
-
 	kit: {
-		outDir: './docs',
-		adapter: adapter(),
-
+		adapter: adapter({
+			pages: 'docs',
+			assets: 'docs'
+		}),
 		prerender: {
 			default: true,
 			entries: ['*']
