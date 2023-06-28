ce73955066367e94dba410247ac6e1b7b6c87083
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2023-06-28 20:18:51 +0530

    add sponsor logos

--- src/lib/components/Sponsors.svelte
+++ src/lib/components/Sponsors.svelte
@@ -2,8 +2,25 @@
 	<div class="flex flex-col gap-2">
 		<div class="font-semibold text-feLightBlue-500 text-base">Funded By The Community</div>
 		<div class="font-bold text-3xl flex items-center gap-4 ">
-			Become A Sponsor
+			Meet Our Sponsors
 		</div>
+		<div class="grid grid-cols-4 gap-2 mt-2">
+			<div class="flex items-center justify-center">
+				<img src="sponsors/01.png" alt="EKBY'S" class="h-auto">
+			</div>
+			<div class="flex items-center justify-center">
+				<a href="https://veefriends.com" target="_blank">
+					<img src="sponsors/02.png" alt="VeeFriends" class="h-auto">
+				</a>
+			</div>
+			<div class="flex items-center justify-center">
+				<img src="sponsors/03.png" alt="Pique Events" class="h-auto">
+			</div>
+			<div class="flex items-center justify-center">
+				<img src="sponsors/04.png" alt="Hungry Minds" class="h-auto">
+			</div>
+		</div>
+		
 		<div class="mt-3 prose">
 			If you find FastEndpoints useful, please consider becoming a sponsor either individually, anonymously 
 			or through your company. There are a few different tiers of sponsorship with and without perks. 
