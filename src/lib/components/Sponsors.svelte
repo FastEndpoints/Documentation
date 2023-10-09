b21f737b199d523da2e021d6434ca77e29eccd61
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2023-10-09 20:02:55 +0530

    update sponsors

--- src/lib/components/Sponsors.svelte
+++ src/lib/components/Sponsors.svelte
@@ -1,31 +1,32 @@
 <section id="contributors">
 	<div class="flex flex-col gap-2">
 		<div class="font-semibold text-feLightBlue-500 text-base">Funded By The Community</div>
-		<div class="font-bold text-3xl flex items-center gap-4 ">
-			Meet Our Sponsors
-		</div>
-		<div class="grid grid-cols-4 gap-2 mt-2">
+		<div class="font-bold text-3xl flex items-center gap-4 ">Meet Our Sponsors</div>
+		<div class="grid grid-cols-4 gap-2 mt-0">
 			<div class="flex items-center justify-center">
-				<img src="sponsors/01.png" alt="EKBY'S" class="h-auto">
+				<img src="sponsors/01.png" alt="EKBY'S" class="h-auto" />
 			</div>
 			<div class="flex items-center justify-center">
 				<a href="https://veefriends.com" target="_blank">
-					<img src="sponsors/02.png" alt="VeeFriends" class="h-auto">
+					<img src="sponsors/veefriends.png" alt="VeeFriends" class="h-auto" />
 				</a>
 			</div>
 			<div class="flex items-center justify-center">
-				<img src="sponsors/03.png" alt="Pique Events" class="h-auto">
+				<a href="https://www.jetbrains.com/" target="_blank">
+					<img src="sponsors/jetbrains.png" alt="JetBrains" class="h-auto" />
+				</a>
 			</div>
 			<div class="flex items-center justify-center">
-				<img src="sponsors/04.png" alt="Hungry Minds" class="h-auto">
+				<img src="sponsors/04.png" alt="Hungry Minds" class="h-auto" />
 			</div>
 		</div>
-		
-		<div class="mt-3 prose">
-			If you find FastEndpoints useful, please consider becoming a sponsor either individually, anonymously 
-			or through your company. There are a few different tiers of sponsorship with and without perks. 
-			Cancel anytime and choose the freqency/amount you'd like to contribute.
-			Please visit our <a href="https://opencollective.com/fast-endpoints">Open Collective page</a> to become a sponsor.
-		</div>		
+
+		<div class="mt-0 prose">
+			If you find FastEndpoints useful, please consider becoming a sponsor either individually,
+			anonymously or through your company. There are a few different tiers of sponsorship with and
+			without perks. Cancel anytime and choose the freqency/amount you'd like to contribute. Please
+			visit our <a href="https://opencollective.com/fast-endpoints">Open Collective page</a> to become
+			a sponsor.
+		</div>
 	</div>
 </section>
