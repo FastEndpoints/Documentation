45e8df06bbbb485f60422c7e08a4bfc641608ff2
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2024-06-09 13:46:35 +0530

    remove discord badge

--- src/lib/components/GithubBadges.svelte
+++ src/lib/components/GithubBadges.svelte
@@ -1,44 +1,43 @@
 <script>
-	import { Button } from '@svelteness/kit-docs';
+    import { Button } from '@svelteness/kit-docs';
 </script>
 
 <div class="flex flex-wrap gap-2 1200:hidden 1280:flex">
-	<Button href="https://github.com/FastEndpoints/Library/blob/main/LICENSE.md">
-		<img
-			src="https://img.shields.io/github/license/dj-nitehawk/FastEndpoints?color=blue&label=license&logo=Github&style=flat-rounded"
-			alt="Github license MIT"
-			class=""
-		/>
-	</Button>
+    <Button href="https://github.com/FastEndpoints/Library/blob/main/LICENSE.md">
+        <img
+            src="https://img.shields.io/github/license/dj-nitehawk/FastEndpoints?color=blue&label=license&logo=Github&style=flat-rounded"
+            alt="Github license MIT"
+            class=""
+        />
+    </Button>
 
-	<Button href="https://www.nuget.org/packages/FastEndpoints">
-		<img
-			src="https://img.shields.io/nuget/v/FastEndpoints?label=version&logo=NuGet&style=flat-rounded"
-			alt="FastEndpoints Nuget Package"
-			class=""
-		/>
-	</Button>
+    <Button href="https://www.nuget.org/packages/FastEndpoints">
+        <img
+            src="https://img.shields.io/nuget/v/FastEndpoints?label=version&logo=NuGet&style=flat-rounded"
+            alt="FastEndpoints Nuget Package"
+            class=""
+        />
+    </Button>
 
-<!-- 	<Button href="https://dev.azure.com/RyanGunner/FastEndpoints/_build/latest?definitionId=7">
-		<img
-			src="https://img.shields.io/azure-devops/tests/RyanGunner/FastEndpoints/7?color=blue&label=tests&logo=Azure%20DevOps&style=flat-rounded"
-			alt="FastEndpoints Test Coverage"
-			class=""
-		/>
-	</Button> -->
+    <!--<Button href="https://dev.azure.com/RyanGunner/FastEndpoints/_build/latest?definitionId=7">-->
+    <!--    <img-->
+    <!--        src="https://img.shields.io/azure-devops/tests/RyanGunner/FastEndpoints/7?color=blue&label=tests&logo=Azure%20DevOps&style=flat-rounded"-->
+    <!--        alt="FastEndpoints Test Coverage"-->
+    <!--        class="" />-->
+    <!--</Button>-->
 
-	<Button href="https://www.nuget.org/packages/FastEndpoints">
-		<img
-			src="https://img.shields.io/nuget/dt/FastEndpoints?color=blue&label=downloads&logo=NuGet&style=flat-rounded"
-			alt="FastEndpoints total downloads"
-			class=""
-		/>
-	</Button>
-	<Button href="https://discord.gg/ARGPxTukpr">
-		<img
-			src="https://img.shields.io/discord/933662816458645504?color=blue&label=discord&logo=discord&logoColor=white&style=flat-rounded"
-			alt="FastEndpoints Discord Online Count"
-			class=""
-		/>
-	</Button>
+    <Button href="https://www.nuget.org/packages/FastEndpoints">
+        <img
+            src="https://img.shields.io/nuget/dt/FastEndpoints?color=blue&label=downloads&logo=NuGet&style=flat-rounded"
+            alt="FastEndpoints total downloads"
+            class=""
+        />
+    </Button>
+    <!--<Button href="https://discord.gg/ARGPxTukpr">-->
+    <!--	<img-->
+    <!--		src="https://img.shields.io/discord/933662816458645504?color=blue&label=discord&logo=discord&logoColor=white&style=flat-rounded"-->
+    <!--		alt="FastEndpoints Discord Online Count"-->
+    <!--		class=""-->
+    <!--	/>-->
+    <!--</Button>-->
 </div>
