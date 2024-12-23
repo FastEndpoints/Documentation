62967f398f6bea9cd72fe0d6f8d39590358bb83c
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2024-12-23 19:10:34 +0530

    complex binding updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,19 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import {sveltekit} from '@sveltejs/kit/vite';
 import kitDocs from '@svelteness/kit-docs/node';
 import icons from 'unplugin-icons/vite';
-import { UserConfig } from 'vite';
+import type {UserConfig} from 'vite';
 
 export default {
-	plugins: [
-		icons({ compiler: 'svelte' }),
-		kitDocs({
-			shiki: {
-				theme: 'one-dark-pro'
-			}
-		}),
-		sveltekit()
-	],
-	optimizeDeps: {
-		include: ["highlight.js", "highlight.js/lib/core"],
-	},
+    plugins: [
+        icons({compiler: 'svelte'}),
+        kitDocs({
+            shiki: {
+                theme: 'one-dark-pro'
+            }
+        }),
+        sveltekit()
+    ],
+    optimizeDeps: {
+        include: ["highlight.js", "highlight.js/lib/core"],
+    },
 } as UserConfig;
