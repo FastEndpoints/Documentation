6b614f4ff63773c96409fd68f3c139baad6de34d
Dĵ ΝιΓΞΗΛψΚ <djnitehawk83@gmail.com>
2026-04-13 21:10:51 +0530

    fix shields

--- src/lib/components/GithubBadges.svelte
+++ src/lib/components/GithubBadges.svelte
@@ -5,7 +5,7 @@
 <div class="flex flex-wrap gap-2 1200:hidden 1280:flex">
     <Button href="https://github.com/FastEndpoints/FastEndpoints/blob/main/LICENSE.md">
         <img
-            src="https://img.shields.io/github/license/FastEndpoints/FastEndpoints?color=blue&label=license&logo=Github&style=flat-circle"
+            src="https://img.shields.io/github/license/FastEndpoints/FastEndpoints?color=blue&label=license&logo=Github&style=flat"
             alt="Github license MIT"
             class=""
         />
@@ -13,7 +13,7 @@
 
     <Button href="https://www.nuget.org/packages/FastEndpoints">
         <img
-            src="https://img.shields.io/nuget/v/FastEndpoints?label=version&logo=NuGet&style=flat-rounded"
+            src="https://img.shields.io/nuget/v/FastEndpoints?label=version&logo=NuGet&style=flat"
             alt="FastEndpoints Nuget Package"
             class=""
         />
@@ -21,7 +21,7 @@
 
     <Button href="https://www.nuget.org/packages/FastEndpoints">
         <img
-            src="https://img.shields.io/nuget/dt/FastEndpoints?color=blue&label=downloads&logo=NuGet&style=flat-rounded"
+            src="https://img.shields.io/nuget/dt/FastEndpoints?color=blue&label=downloads&logo=NuGet&style=flat"
             alt="FastEndpoints total downloads"
             class=""
         />
